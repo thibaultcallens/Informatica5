08331d717868247c3e8d88272d6366187097a09c
thibaultcallens <43027861+thibaultcallens@users.noreply.github.com>
2018-10-07 10:59:07 +0200

    Pythagoras afgewerkt

---
+++ 05-Eenvoudige functies/Pythagoras.py
@@ -0,0 +1,9 @@
+from math import sqrt
+a = float(input('a = '))
+b = float(input('b = '))
+c = sqrt((a ** 2) + (b ** 2))
+
+print('In een rechthoekige driehoek met rechthoekszijden a = ' + str(round(a, 2)) + ' en b = ' + str(round(b, 2)) + ' is de schuine zijde ' + str(round(c, 2)))
+
+
+
