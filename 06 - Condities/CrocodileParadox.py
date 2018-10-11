dc2c4e2ec07ad13988b068839d6f34d597b46d34
thibaultcallens <43027861+thibaultcallens@users.noreply.github.com>
2018-10-11 09:56:16 +0200

    Crocodile Paradox afgewerkt

---
+++ 06 - Condities/CrocodileParadox.py
@@ -0,0 +1,8 @@
+stalen_zenuwen = input('Wie heeft stalen zenuwen? ' )
+
+if stalen_zenuwen == 'vader':
+    print('Krokodil geeft kind terug')
+elif stalen_zenuwen == 'krokodil':
+    print('Krokodil eet kind op')
+else:
+    print('Moe van het denken')
