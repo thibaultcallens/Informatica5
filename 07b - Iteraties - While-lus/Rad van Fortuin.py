863122010f369099e7c60f141b2c0c1a2e07c25e
thibaultcallens <43027861+thibaultcallens@users.noreply.github.com>
2018-11-20 18:14:32 +0100

    Rad van Fortuin afgewerkt

--- 07b - Iteraties - While-lus/Rad van Fortuin.py
+++ 07b - Iteraties - While-lus/Rad van Fortuin.py
@@ -2,9 +2,11 @@ woord = str(input('Geef het woord: '))
 gedraaide_geldbedrag = int(input('Geef bedrag: '))
 bedrag = 0
 gegokte_letter = str(input('Geef gegokte letter: '))
+vreeself = ''
 
-while gegokte_letter in woord:
+while gegokte_letter in woord and gegokte_letter not in vreeself:
     bedrag += gedraaide_geldbedrag
+    vreeself += gegokte_letter
     gegokte_letter = str(input('Geef gegokte letter: '))
 
 print(bedrag)
