d3280ecaf310f785d817f1676ddbc09c362ae5ee
thibaultcallens <43027861+thibaultcallens@users.noreply.github.com>
2018-11-29 09:14:57 +0100

    Unieke basen afgewerkt

--- Toets/Unieke basen.py
+++ Toets/Unieke basen.py
@@ -6,13 +6,13 @@ for i in range(1, aantal):
     base = str(input('Geef een base: '))
     soorten_basen += basen
     if base not in soorten_basen:
-        basen += base
+        basen = basen + ' ' + base
 
-if len(basen) == 4:
+if len(basen) - 3 == 4:
     mes = 'De DNA-keting bevat 4 verschillende soorten basen: {} '.format(basen)
-elif len(basen) == 3:
+elif len(basen) - 2 == 3:
     mes = 'De DNA-keting bevat 3 verschillende soorten basen: {} '.format(basen)
-elif len(basen) == 2:
+elif len(basen) - 1 == 2:
     mes = 'De DNA-keting bevat 2 verschillende soorten basen: {} '.format(basen)
 else:
     mes = 'De DNA-keting bevat 1 soort base: {} '.format(basen)
